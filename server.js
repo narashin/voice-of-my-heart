44a3d8ccd5e849842459d0b296412d49c85c1847
uselesscarrot <whatauseless@gmail.com>
2016-11-04 21:35:28 +0900

    변수명 수정

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var token = '276043456:AAHzIvPoLp54IXewyAhk6yby_vCB7d8J88s';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/insult/, function(msg, match) {
+bot.onText(/...../, function(msg, match) {
         var fromId = msg.from.id;
         var vomhMessages = ["멍멍", "사회생활 어렵다", "?????"];
         var chosenVomhMessages = [Math.floor(Math.random() * vomhMessages.length)];
