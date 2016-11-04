b5e7296dd53bf3b878aa971bafef99eb0ec4f8ce
uselesscarrot <whatauseless@gmail.com>
2016-11-04 21:42:46 +0900

    오타수정

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var TelegramBot = require('node-telegram-bot-api');
-
 var token = '276043456:AAHzIvPoLp54IXewyAhk6yby_vCB7d8J88s';
-// Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/...../, function(msg, match) {
         var fromId = msg.from.id;
         var vomhMessages = ["멍멍", "사회생활 어렵다", "?????"];
-        var chosenVomhMessages = [Math.floor(Math.random() * vomhMessages.length)];
+        var chosenVomhMessages = vomhMessages[Math.floor(Math.random() * vomhMessages.length)];
         bot.sendMessage(fromId, chosenVomhMessages);
 });
 
