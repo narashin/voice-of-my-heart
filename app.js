718ee96b7a0ddab760388e5f17fd36f5937b402a
uselesscarrot <whatauseless@gmail.com>
2016-11-06 13:50:29 +0900

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-          res.status(200).send('Hello, world!');
+          res.status(200).send('server.js를 보고싶다');
           });
 
 if (module === require.main) {
